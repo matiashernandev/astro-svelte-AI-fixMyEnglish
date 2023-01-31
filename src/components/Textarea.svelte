f05d1735c654365e543fd7997cee8070c513750d
nkwaaaa <nkwaaaa@gmail.com>
2023-01-31 14:59:02 -0300

    deploy

--- src/components/Textarea.svelte
+++ src/components/Textarea.svelte
@@ -20,7 +20,7 @@
   on:input={handleChange}
   id="result"
   class="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-gray-300 rounded-lg appearance-none focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-  placeholder="Put your english to fix"
+  placeholder="Put your english to fix e.g 'i are Hernán'"
   name="comment"
   rows="5"
   cols="40"
